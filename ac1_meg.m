4ac435bb824742999a6aeb5a5b93452cac5768fc
Scott Burns <scott.s.burns@gmail.com>
2011-02-10 11:54:22 -0500

    VTSD.m:changed blink to new blink text

---
+++ ac1_meg.m
@@ -0,0 +1,55 @@
+VTSD('Blink','ac1','MEG',101,1);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('ATLLoc','ac1','MEG',101,1);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('MaskedMM','ac1','MEG',101,1);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('MaskedMM','ac1','MEG',101,2);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,1);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,2);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,3);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,4);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,5);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,6);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,7);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('BaleenMM','ac1','MEG',101,8);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('AXCPT','ac1','MEG',101,1);
+fprintf('\nPress any key to continue to the next experiment...');
+KbStrokeWait;
+
+VTSD('AXCPT','ac1','MEG',101,2);
+fprintf('\nYou have finished, thanks for participating...');
+
